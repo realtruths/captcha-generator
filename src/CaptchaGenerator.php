96b13a0f64089e9c77291c6d0eff883ef0d12d97
Jeremy <realtruths@126.com>
2021-08-10 18:44:22 +0800

    update

--- src/CaptchaGenerator.php
+++ src/CaptchaGenerator.php
@@ -1,6 +1,4 @@
 <?php
-
-declare(strict_types=1);
 /**
  * CaptchaGenerator.php
  *
@@ -12,6 +10,7 @@ namespace RealTruths\Captcha\Generator;
 
 /**
  * Class CaptchaGenerator
+ * @package RealTruths\Captcha\Generator
  */
 class CaptchaGenerator
 {
